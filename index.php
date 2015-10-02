c1dee465b779dd0a949c97f589c87214e1afec61
Cesar Campos <cesar.campos@navigomining.com>
2015-10-02 08:22:00 -0300

    last changes

--- index.php
+++ index.php
@@ -1,230 +1,23 @@
 <!DOCTYPE html>
-<!--
-Template: Metronic Frontend Freebie - Responsive HTML Template Based On Twitter Bootstrap 3.3.4
-Version: 1.0.0
-Author: KeenThemes
-Website: http://www.keenthemes.com/
-Contact: support@keenthemes.com
-Follow: www.twitter.com/keenthemes
-Like: www.facebook.com/keenthemes
-Purchase Premium Metronic Admin Theme: http://themeforest.net/item/metronic-responsive-admin-dashboard-template/4021469?ref=keenthemes
--->
 <!--[if IE 8]> <html lang="en" class="ie8 no-js"> <![endif]-->
 <!--[if IE 9]> <html lang="en" class="ie9 no-js"> <![endif]-->
 <!--[if !IE]><!-->
-<html lang="en">
+<html lang="es">
 <!--<![endif]-->
 
 <!-- Head BEGIN -->
-<head>
-  <meta charset="utf-8">
-  <title>Hotel Hain</title>
+<?php $page_description = "Home of Hain";
+      $page_title = "Hotel Hain - Home" ;
+      include("includes/_header.php"); ?>
 
-  <meta content="width=device-width, initial-scale=1.0" name="viewport">
-  <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
-
-  <meta content="Metronic Shop UI description" name="description">
-  <meta content="Metronic Shop UI keywords" name="keywords">
-  <meta content="keenthemes" name="author">
-
-  <meta property="og:site_name" content="-CUSTOMER VALUE-">
-  <meta property="og:title" content="-CUSTOMER VALUE-">
-  <meta property="og:description" content="-CUSTOMER VALUE-">
-  <meta property="og:type" content="website">
-  <meta property="og:image" content="-CUSTOMER VALUE-"><!-- link to image for socio -->
-  <meta property="og:url" content="-CUSTOMER VALUE-">
-
-  <link rel="shortcut icon" href="favicon.ico">
-
-  <!-- Fonts START -->
-  <link href="http://fonts.googleapis.com/css?family=Open+Sans:300,400,600,700|PT+Sans+Narrow|Source+Sans+Pro:200,300,400,600,700,900&amp;subset=all" rel="stylesheet" type="text/css">
-  <!-- Fonts END -->
-
-  <!-- Global styles START -->          
-  <link href="assets/plugins/font-awesome/css/font-awesome.min.css" rel="stylesheet">
-  <link href="assets/plugins/bootstrap/css/bootstrap.min.css" rel="stylesheet">
-  <!-- Global styles END --> 
-   
-  <!-- Page level plugin styles START -->
-  <link href="assets/pages/css/animate.css" rel="stylesheet">
-  <link href="assets/plugins/fancybox/source/jquery.fancybox.css" rel="stylesheet">
-  <link href="assets/plugins/owl.carousel/assets/owl.carousel.css" rel="stylesheet">
-  <!-- Page level plugin styles END -->
-
-  <!-- Theme styles START -->
-  <link href="assets/pages/css/components.css" rel="stylesheet">
-  <link href="assets/pages/css/slider.css" rel="stylesheet">
-  <link href="assets/corporate/css/style.css" rel="stylesheet">
-  <link href="assets/corporate/css/style-responsive.css" rel="stylesheet">
-  <link href="assets/corporate/css/themes/red.css" rel="stylesheet" id="style-color">
-  <link href="assets/corporate/css/custom.css" rel="stylesheet">
-  <!-- Theme styles END -->
-</head>
 <!-- Head END -->
 
 <!-- Body BEGIN -->
 <body class="corporate">
-    <!-- BEGIN STYLE CUSTOMIZER -->
-    <div class="color-panel hidden-sm hidden">
-      <div class="color-mode-icons icon-color"></div>
-      <div class="color-mode-icons icon-color-close"></div>
-      <div class="color-mode">
-        <p>THEME COLOR</p>
-        <ul class="inline">
-          <li class="color-red current color-default" data-style="red"></li>
-          <li class="color-blue" data-style="blue"></li>
-          <li class="color-green" data-style="green"></li>
-          <li class="color-orange" data-style="orange"></li>
-          <li class="color-gray" data-style="gray"></li>
-          <li class="color-turquoise" data-style="turquoise"></li>
-        </ul>
-      </div>
-    </div>
-    <!-- END BEGIN STYLE CUSTOMIZER --> 
 
-    <!-- BEGIN TOP BAR -->
-    <div class="pre-header">
-        <div class="container">
-            <div class="row">
-                <!-- BEGIN TOP BAR LEFT PART -->
-                <div class="col-md-6 col-sm-6 additional-shop-info">
-                    <ul class="list-unstyled list-inline">
-                        <li><i class="fa fa-phone"></i><span>+56 612241357</span></li>
-                        <li><i class="fa fa-envelope-o"></i><span>hotelhain@gmail.com</span></li>
-                    </ul>
-                </div>
-                <!-- END TOP BAR LEFT PART -->
-                <!-- BEGIN TOP BAR MENU -->
-                <div class="col-md-6 col-sm-6 additional-nav hidden">
-                    <ul class="list-unstyled list-inline pull-right">
-                        <li><a href="page-login.html">Log In</a></li>
-                        <li><a href="page-reg-page.html">Registration</a></li>
-                    </ul>
-                </div>
-                <!-- END TOP BAR MENU -->
-            </div>
-        </div>        
-    </div>
-    <!-- END TOP BAR -->
-    <!-- BEGIN HEADER -->
-    <div class="header">
-      <div class="container">
-        <a class="site-logo" href="index.html"><img src="assets/corporate/img/logos/logo-corp-red.png" alt="Metronic FrontEnd"></a>
-
-        <a href="javascript:void(0);" class="mobi-toggler"><i class="fa fa-bars"></i></a>
-
-        <!-- BEGIN NAVIGATION -->
-        <div class="header-navigation pull-right font-transform-inherit">
-          <ul>
-            <li class="dropdown active">
-              <a class="dropdown-toggle" data-toggle="dropdown" data-target="#" href="javascript:;">
-                Home 
-                
-              </a>
-                
-              <ul class="dropdown-menu">
-                <li><a href="index.html">Home Default</a></li>
-                <li class="active"><a href="index-header-fix.html">Home with Header Fixed</a></li>
-                <li><a href="index-without-topbar.html">Home without Top Bar</a></li>
-              </ul>
-            </li>
-
-            <li class="dropdown">
-              <a class="dropdown-toggle" data-toggle="dropdown" data-target="#" href="javascript:;">
-                El Hotel 
-              </a>
-              <ul class="dropdown-menu">
-                <li><a href="page-services.html">Servicios</a></li>
-                <li><a href="page-rooms.html">Habitaciones</a></li>
-                <li><a href="page-prices.html">Tarifas</a></li>
-                <li><a href="page-reservations.html">Reservas</a></li>
-                <li><a href="page-contacts.html">Ubicación</a></li>
-              </ul>
-            </li>
-
-            <li>
-              <a href="page-travel.html">Viajes del Hain
-              </a>
-            </li>
-
-            <li>
-              <a href="page-gastronomy.html">Gastronomía
-              </a>
-            </li>
-            <li>
-              <a href="page-promos.html">Promociones
-              </a>
-            </li>
-
-            <li class="dropdown">
-              <a class="dropdown-toggle" data-toggle="dropdown" data-target="#" href="javascript:;">
-                Pages 
-                
-              </a>
-              <ul class="dropdown-menu">
-                <li><a href="page-about.html">About Us</a></li>
-                <li><a href="page-services.html">Services</a></li>
-                <li><a href="page-prices.html">Prices</a></li>
-                <li><a href="page-faq.html">FAQ</a></li>
-                <li><a href="page-gallery.html">Gallery</a></li>
-                <li><a href="page-search-result.html">Search Result</a></li>
-                <li><a href="page-404.html">404</a></li>
-                <li><a href="page-500.html">500</a></li>
-                <li><a href="page-login.html">Login Page</a></li>
-                <li><a href="page-forgotton-password.html">Forget Password</a></li>
-                <li><a href="page-reg-page.html">Signup Page</a></li>
-                <li><a href="page-careers.html">Careers</a></li>
-                <li><a href="page-site-map.html">Site Map</a></li>
-                <li><a href="page-contacts.html">Contact</a></li>                
-              </ul>
-            </li>
-
-            <li class="dropdown">
-              <a class="dropdown-toggle" data-toggle="dropdown" data-target="#" href="javascript:;">
-                Fotos
-              </a>
-                
-              <ul class="dropdown-menu">
-                <li><a href="portfolio-4.html">Portfolio 4</a></li>
-                <li><a href="portfolio-3.html">Portfolio 3</a></li>
-                <li><a href="portfolio-2.html">Portfolio 2</a></li>
-                <li><a href="portfolio-item.html">Portfolio Item</a></li>
-              </ul>
-            </li>
-            <li class="dropdown">
-              <a class="dropdown-toggle" data-toggle="dropdown" data-target="#" href="javascript:;">
-                Blog 
-                
-              </a>
-                
-              <ul class="dropdown-menu">
-                <li><a href="blog.html">Blog Page</a></li>
-                <li><a href="blog-item.html">Blog Item</a></li>
-              </ul>
-            </li>
-
-            <!-- BEGIN TOP SEARCH -->
-            <li class="menu-search hidden">
-              <span class="sep"></span>
-              <i class="fa fa-search search-btn"></i>
-              <div class="search-box">
-                <form action="#">
-                  <div class="input-group">
-                    <input type="text" placeholder="Search" class="form-control">
-                    <span class="input-group-btn">
-                      <button class="btn btn-primary" type="submit">Search</button>
-                    </span>
-                  </div>
-                </form>
-              </div> 
-            </li>
-            <!-- END TOP SEARCH -->
-          </ul>
-        </div>
-        <!-- END NAVIGATION -->
-      </div>
-    </div>
-    <!-- Header END -->
+    <!-- NAV HEADER -->
+    <?php include("includes/_nav.html"); ?>
+    <!-- NAV Header END -->
 
     <!-- BEGIN SLIDER -->
     <div class="page-slider margin-bottom-40">
@@ -599,91 +392,9 @@ Purchase Premium Metronic Admin Theme: http://themeforest.net/item/metronic-resp
       </div>
     </div>
 
-    <!-- BEGIN PRE-FOOTER -->
-    <div class="pre-footer">
-      <div class="container">
-        <div class="row">
-          <!-- BEGIN BOTTOM ABOUT BLOCK -->
-          <div class="col-md-4 col-sm-6 pre-footer-col">
-            <h2>About us</h2>
-            <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam sit nonummy nibh euismod tincidunt ut laoreet dolore magna aliquarm erat sit volutpat.</p>
-
-            <div class="photo-stream">
-              <h2>Photos Stream</h2>
-              <ul class="list-unstyled">
-                <li><a href="javascript:;"><img alt="" src="assets/pages/img/people/img5-small.jpg"></a></li>
-                <li><a href="javascript:;"><img alt="" src="assets/pages/img/works/img1.jpg"></a></li>
-                <li><a href="javascript:;"><img alt="" src="assets/pages/img/people/img4-large.jpg"></a></li>
-                <li><a href="javascript:;"><img alt="" src="assets/pages/img/works/img6.jpg"></a></li>
-                <li><a href="javascript:;"><img alt="" src="assets/pages/img/works/img3.jpg"></a></li>
-                <li><a href="javascript:;"><img alt="" src="assets/pages/img/people/img2-large.jpg"></a></li>
-                <li><a href="javascript:;"><img alt="" src="assets/pages/img/works/img2.jpg"></a></li>
-                <li><a href="javascript:;"><img alt="" src="assets/pages/img/works/img5.jpg"></a></li>
-                <li><a href="javascript:;"><img alt="" src="assets/pages/img/people/img5-small.jpg"></a></li>
-                <li><a href="javascript:;"><img alt="" src="assets/pages/img/works/img1.jpg"></a></li>
-                <li><a href="javascript:;"><img alt="" src="assets/pages/img/people/img4-large.jpg"></a></li>
-                <li><a href="javascript:;"><img alt="" src="assets/pages/img/works/img6.jpg"></a></li>
-                <li><a href="javascript:;"><img alt="" src="assets/pages/img/works/img3.jpg"></a></li>
-                <li><a href="javascript:;"><img alt="" src="assets/pages/img/people/img2-large.jpg"></a></li>
-                <li><a href="javascript:;"><img alt="" src="assets/pages/img/works/img2.jpg"></a></li>
-              </ul>                    
-            </div>
-          </div>
-          <!-- END BOTTOM ABOUT BLOCK -->
-
-          <!-- BEGIN BOTTOM CONTACTS -->
-          <div class="col-md-4 col-sm-6 pre-footer-col">
-            <h2>Our Contacts</h2>
-            <address class="margin-bottom-40">
-              Jose Nogueira 1600 <br>
-              Punta Arenas, Magallanes, Chile<br>
-              Phone: +56 612241357<br>
-              ZIP Code: 6200000 <br>
-              Email: <a href="mailto:hotelhain@gmail.com">hotelhain@gmail.com</a><br>
-            </address>
-
-          </div>
-          <!-- END BOTTOM CONTACTS -->
-
-          <!-- BEGIN TWITTER BLOCK --> 
-          <div class="col-md-4 col-sm-6 pre-footer-col">
-            <h2 class="margin-bottom-0">Latest Tweets</h2>
-            <a class="twitter-timeline" href="https://twitter.com/twitterapi" data-tweet-limit="2" data-theme="dark" data-link-color="#57C8EB" data-widget-id="455411516829736961" data-chrome="noheader nofooter noscrollbar noborders transparent">Loading tweets by @keenthemes...</a>
-          </div>
-          <!-- END TWITTER BLOCK -->
-        </div>
-      </div>
-    </div>
-    <!-- END PRE-FOOTER -->
-
-    <!-- BEGIN FOOTER -->
-    <div class="footer">
-      <div class="container">
-        <div class="row">
-          <!-- BEGIN COPYRIGHT -->
-          <div class="col-md-6 col-sm-6 padding-top-10">
-            2015 © Keenthemes. ALL Rights Reserved. <a href="javascript:;">Privacy Policy</a> | <a href="javascript:;">Terms of Service</a>
-          </div>
-          <!-- END COPYRIGHT -->
-          <!-- BEGIN PAYMENTS -->
-          <div class="col-md-6 col-sm-6">
-            <ul class="social-footer list-unstyled list-inline pull-right">
-              <li><a href="javascript:;"><i class="fa fa-facebook"></i></a></li>
-              <li><a href="javascript:;"><i class="fa fa-google-plus"></i></a></li>
-              <li><a href="javascript:;"><i class="fa fa-dribbble"></i></a></li>
-              <li><a href="javascript:;"><i class="fa fa-linkedin"></i></a></li>
-              <li><a href="javascript:;"><i class="fa fa-twitter"></i></a></li>
-              <li><a href="javascript:;"><i class="fa fa-skype"></i></a></li>
-              <li><a href="javascript:;"><i class="fa fa-github"></i></a></li>
-              <li><a href="javascript:;"><i class="fa fa-youtube"></i></a></li>
-              <li><a href="javascript:;"><i class="fa fa-dropbox"></i></a></li>
-            </ul>  
-          </div>
-          <!-- END PAYMENTS -->
-        </div>
-      </div>
-    </div>
-    <!-- END FOOTER -->
+    <!-- BEGIN FOOTER BLOCK -->
+    <?php include("includes/_footer.html"); ?>
+    <!-- END FOOTER BLOCK -->
 
     <!-- Load javascripts at bottom, this will reduce page load time -->
     <!-- BEGIN CORE PLUGINS (REQUIRED FOR ALL PAGES) -->
---
+++ page-prices.php
@@ -0,0 +1,244 @@
+<!DOCTYPE html>
+<!--[if IE 8]> <html lang="en" class="ie8 no-js"> <![endif]-->
+<!--[if IE 9]> <html lang="en" class="ie9 no-js"> <![endif]-->
+<!--[if !IE]><!-->
+<html lang="es">
+<!--<![endif]-->
+
+<!-- Head BEGIN -->
+<?php $page_description = "Prices of Hain Rooms";
+      $page_title = "Hotel Hain - Precios" ;
+      include("includes/_header.php");?>
+<!-- Head END -->
+
+<!-- Body BEGIN -->
+<body class="corporate">
+
+    <!-- NAV HEADER -->
+    <?php include("includes/_nav.html"); ?>
+    <!-- NAV Header END -->
+
+
+    <div class="main">
+      <div class="container">
+        <ul class="breadcrumb">
+            <li><a href="index.html">Home</a></li>
+            <li><a href="javascript:;">Hotel</a></li>
+            <li class="active">Tarifas</li>
+        </ul>
+        <!-- BEGIN CONTENT -->
+
+        <div class="row margin-bottom-40">
+          <!-- BEGIN CONTENT -->
+          <div class="col-md-12 col-sm-12">
+            <div class="row"> 
+              <div class="col-md-6 col-md-offset-3 why-hain">
+              <h1>Tarifas</h1>
+              <h2></h2>
+              <h4>Condiciones</h4>
+              <p>
+                <ul class="pricing-content list-unstyled listing-conditions">
+                  <li>
+                    <i class="fa fa-usd"></i> Tarifas en USD no incluyen impuesto (19% de Iva).
+                  </li>
+                  <li>
+                    <i class="fa fa-coffee"></i> Desayuno Buffet (servido entre 7 y 10 AM) incluido.
+                  </li>
+                  <li>
+                    <i class="fa fa-wifi"></i> Internet Wi Fi incluida.
+                  </li>
+                  <li>
+                    <i class="fa fa-building-o"></i> Inicio Check in: 13.00 hrs.
+                  </li>
+                  <li>
+                    <i class="fa fa-taxi"></i> Termino Check Out: 11.00 hrs.
+                  </li>
+                </ul>
+              </p>
+              <p>
+                Temporada Baja: Desde 16 de Abril de 2016 Hasta 14 de Octubre de 2016 <br>
+                Temporada Alta: Desde 15 de Octubre de 2016 Hasta  15 de Abril de 2016<br>
+                Toda reserva está sujeta a las condiciones establecidas en <a href="page-reserves">nuestra política de reservas</a>
+              </p>
+              </div>
+            </div>
+            <div class="content-page">
+              <div class="row">
+                <div class="col-md-4">
+                  <div class="">
+                    <div class="pricing-head">
+                      <h3>Single
+                        <span>
+                           Viajes de negocios
+                        </span>
+                      </h3>
+                      <h4><i>$</i>30.000
+                        <span>
+                           Temporada Baja
+                        </span>
+                      </h4>
+                      <h4><i>$</i>33.000
+                        <span>
+                           Temporada Alta
+                        </span>
+                      </h4>
+                    </div>
+                    <ul class="pricing-content list-unstyled">
+
+                    </ul>
+                    <div class="pricing-footer">
+
+                    </div>
+                  </div>
+                </div>
+
+                <div class="col-md-4">
+                  <div class="">
+                    <div class="pricing-head">
+                      <h3>Matrimonial
+                        <span>
+                           Viajes de pareja
+                        </span>
+                      </h3>
+                      <h4><i>$</i>33.600
+                        <span>
+                           Temporada Baja
+                        </span>
+                      </h4>
+                      <h4><i>$</i>42.600
+                        <span>
+                           Temporada Alta
+                        </span>
+                      </h4>
+                    </div>
+                    <ul class="pricing-content list-unstyled">
+
+                    </ul>
+                    <div class="pricing-footer">
+
+                    </div>
+                  </div>
+                </div>
+                <div class="col-md-4">
+                  <div class="">
+                    <div class="pricing-head">
+                      <h3>Twin Doble
+                        <span>
+                           Viajes de amigos
+                        </span>
+                      </h3>
+                      <h4><i>$</i>33.600
+                        <span>
+                           Temporada Baja
+                        </span>
+                      </h4>
+                      <h4><i>$</i>42.600
+                        <span>
+                           Temporada Alta
+                        </span>
+                      </h4>
+                    </div>
+                    <ul class="pricing-content list-unstyled">
+
+                    </ul>
+                    <div class="pricing-footer">
+
+                    </div>
+                  </div>
+                </div>
+              </div>
+
+              <div class="row margin-bottom-40">
+                <div class="col-md-4">
+                  <div class="">
+                    <div class="pricing-head">
+                      <h3>Triple
+                        <span>
+                           Viaje familiar
+                        </span>
+                      </h3>
+                      <h4><i>$</i>43.800
+                        <span>
+                           Temporada Baja
+                        </span>
+                      </h4>
+                      <h4><i>$</i>54.000
+                        <span>
+                           Temporada Alta
+                        </span>
+                      </h4>
+                    </div>
+                    <ul class="pricing-content list-unstyled">
+
+                    </ul>
+                    <div class="pricing-footer">
+
+                    </div>
+                  </div>
+                </div>
+                <div class="col-md-4">
+                  <div class="">
+                    <div class="pricing-head">
+                      <h3>Triple
+                        <span>
+                           Viaje familiar
+                        </span>
+                      </h3>
+                      <h4><i>$</i>50.000
+                        <span>
+                           Temporada Baja
+                        </span>
+                      </h4>
+                      <h4><i>$</i>60.000
+                        <span>
+                           Temporada Alta
+                        </span>
+                      </h4>
+                    </div>
+                    <ul class="pricing-content list-unstyled">
+
+                    </ul>
+                    <div class="pricing-footer">
+
+                    </div>
+                  </div>
+                </div>
+              </div>
+
+
+            </div>
+          </div>
+        </div>
+        <!-- END CONTENT -->
+      </div>
+    </div>
+
+    <!-- BEGIN FOOTER BLOCK -->
+    <?php include("includes/_footer.html"); ?>
+    <!-- END FOOTER BLOCK -->
+
+    <!-- Load javascripts at bottom, this will reduce page load time -->
+    <!-- BEGIN CORE PLUGINS (REQUIRED FOR ALL PAGES) -->
+    <!--[if lt IE 9]>
+    <script src="assets/plugins/respond.min.js"></script>
+    <![endif]--> 
+    <script src="assets/plugins/jquery.min.js" type="text/javascript"></script>
+    <script src="assets/plugins/jquery-migrate.min.js" type="text/javascript"></script>
+    <script src="assets/plugins/bootstrap/js/bootstrap.min.js" type="text/javascript"></script>      
+    <script src="assets/corporate/scripts/back-to-top.js" type="text/javascript"></script>
+    <!-- END CORE PLUGINS -->
+
+    <!-- BEGIN PAGE LEVEL JAVASCRIPTS (REQUIRED ONLY FOR CURRENT PAGE) -->
+    <script src="assets/plugins/fancybox/source/jquery.fancybox.pack.js" type="text/javascript"></script><!-- pop up -->
+
+    <script src="assets/corporate/scripts/layout.js" type="text/javascript"></script>
+    <script type="text/javascript">
+        jQuery(document).ready(function() {
+            Layout.init();    
+            Layout.initTwitter();
+        });
+    </script>
+    <!-- END PAGE LEVEL JAVASCRIPTS -->
+</body>
+<!-- END BODY -->
+</html>
